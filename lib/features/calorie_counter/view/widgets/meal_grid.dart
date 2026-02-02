6f47995c198c980bf89f004a5777b4d72cf985f6
mariurmm <m.bendyuk7@gmail.com>
2026-02-02 11:13:05 +0500

    ui

---
+++ lib/features/calorie_counter/view/widgets/meal_grid.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'meal_card.dart';
+
+class MealGrid extends StatelessWidget {
+  final int itemCount;
+
+  const MealGrid({
+    super.key,
+    this.itemCount = 6, // временно
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: itemCount,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12,
+        childAspectRatio: 0.9,
+      ),
+      itemBuilder: (context, index) {
+        return const MealCard(
+          imagePath: null,
+          calories: 0,
+        );
+      },
+    );
+  }
+}
